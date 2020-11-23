5ab62ce414025ee2817d3c6b0df650a77c7597d5
Andrew Casey <60218234+AndrewC20@users.noreply.github.com>
2020-11-23 10:45:02 +0000

    Added Song

--- OOP_Exercise_Week9/Program.cs
+++ OOP_Exercise_Week9/Program.cs
@@ -10,6 +10,11 @@ namespace OOP_Exercise_Week9
     {
         static void Main(string[] args)
         {
+            Song s1 = new Song("ED SHEERAN", "BEAUTIFUL PEOPLE", 3.15, Genre.Pop);
+            Song s2 = new Song("REGARD", "RIDE IT", 3.37, Genre.Dance);
+            Song s3 = new Song("TONES & I", "DANCE MONKEY", 4.20, Genre.Dance);
+            Song s4 = new Song("POST MALONE", "CIRCLES", 3.25, Genre.Pop);
+            Song s5 = new Song("ED SHEERAN", "SOUTH OF THE BORDER", 4.26, Genre.Other);
         }
     }
 }
---
+++ OOP_Exercise_Week9/Song.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Exercise_Week9
+{
+    class Song
+    {      
+        public string Title { get; set; }
+
+        public string Artist { get; set; }
+        public double Duration { get; set; }
+       
+        public Genre MusicGenre { get; set; }
+
+        public Song(string title, string artist, double duration, Genre musicGenre)
+        {
+            Title = title;
+            Artist = artist;
+            Duration = duration;
+            MusicGenre = musicGenre;
+        }
+        public Song(string title, string artist) : this(title, artist, 0, Genre.Other) { }
+
+        public Song() : this("Unknown", "Unknown") { }
+
+        public override string ToString()
+        {
+            return string.Format($"{Title} {Artist} {Duration} {MusicGenre}").ToString();
+        }
+    }
+
+    public enum Genre { Rock, Pop, Dance, Other }
+}
